a1050656a5c393e8e74de8e80ee5fe9efafa6bc0
sieky <geromesiekytounkara@gmail.com>
2023-09-26 00:35:36 +0000

    dasboard and login templates

--- src/managol/settings.py
+++ src/managol/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,7 +44,7 @@ ROOT_URLCONF = 'managol.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +105,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ src/managol/views.py
@@ -0,0 +1,9 @@
+from  django.shortcuts import render, redirect
+
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def login(request):
+    return render(request, 'login.html')
